3dbce091d6486777678d2cbc12daf03cb7ab05c4
SvdNeo <sanjay.d@neoito.com>
2023-12-19 20:24:43 +0530

    getting values of linked list by using loop

--- recurrsion.js
+++ recurrsion.js
@@ -71,3 +71,27 @@ function fibb(n){
 }
 console.log(fibb(77))
 
+//task4 linked list 
+let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+
+function printList(list){
+    
+    while(list){
+        console.log(list.value)
+        list = list.next
+
+    }
+}
+printList(list)
