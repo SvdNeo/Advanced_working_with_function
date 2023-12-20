e8e72931c9cde8cf08d93ac26ac821bb26ec885e
SvdNeo <sanjay.d@neoito.com>
2023-12-20 21:21:17 +0530

    example on nested interval

--- setTimeout_setInterval.js
+++ setTimeout_setInterval.js
@@ -20,8 +20,32 @@ clearInterval(timed)
 
 
 //Nested setTimeout
-let timer = setTimeout(function tick(){
-    console.log("times up!")
-    timer = setTimeout(tick,2000)
-},2000)
+// let timer = setTimeout(function tick(){
+//     console.log("times up!")
+//     timer = setTimeout(tick,2000)
+// },2000)
 
+//example on advantage of Nested setTimeout 
+function requestData() {
+    // Simulating a request to the server
+    console.log("Requesting data from the server...");
+  
+    // Assume some asynchronous operation, like an AJAX request
+    // ...
+  
+    // After the request is complete, schedule the next request
+    let nextInterval = calculateNextInterval();
+    setTimeout(requestData, nextInterval);
+  }
+  
+  function calculateNextInterval() {
+    // Here you can implement your logic to calculate the next interval dynamically
+    // For example, you can double the previous interval
+    return Math.min(2 * (lastInterval || 5000), 60000); // Maximum interval of 1 minute
+  }
+  
+  let lastInterval; // Variable to store the last interval
+  
+  // Initial request
+  setTimeout(requestData, 1000);
+  
