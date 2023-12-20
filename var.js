c1a515a7004d852833a767fac6cc5fdd532af9ec
SvdNeo <sanjay.d@neoito.com>
2023-12-20 15:45:36 +0530

    hoisting of var intialization

--- var.js
+++ var.js
@@ -9,4 +9,22 @@ function sayHi() {
   sayHi()
  //console.log(phrase);
  // var is not block scoped but it is function scoped therefore first clg works where as clg outside the function doesnt work
- 
+ function sayHi() {
+    phrase = "Hello";
+  
+   console.log(phrase);
+  
+    var phrase;
+  }
+  sayHi();
+
+  function sayHi() {
+    phrase = "Hello"; // (*)
+  
+    if (false) {
+      var phrase;
+    }
+  
+   console.log(phrase);
+  }
+  sayHi();
