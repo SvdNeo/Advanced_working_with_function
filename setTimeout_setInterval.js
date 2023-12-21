58ee8eecec0736f5c64bc52b1c25a66487c4e878
SvdNeo <sanjay.d@neoito.com>
2023-12-21 10:00:42 +0530

    task2 on setTimeout

--- setTimeout_setInterval.js
+++ setTimeout_setInterval.js
@@ -75,3 +75,13 @@ function requestData() {
     })
   }
 printNum(1,4)
+
+//task 2
+let i = 0;
+
+setTimeout(() => console.log(i), 100); // ?
+
+// assume that the time to execute this function is >100ms
+for(let j = 0; j < 100000000; j++) {
+  i++;
+}
