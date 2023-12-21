0fdc0266e59fe42eaae5bf2ff1331da716f974fb
SvdNeo <sanjay.d@neoito.com>
2023-12-21 20:34:45 +0530

    updated User function with set value

--- getter_setter.js
+++ getter_setter.js
@@ -14,13 +14,20 @@ console.log(user.name);
 function User(name,birthday){
     this.name = name;
     this.birthday = birthday;
-    Object.defineProperty(this,"age",{
-        get(){
-            let a= new Date().getFullYear() - this.birthday.getFullYear();
-            return a
+    Object.defineProperty(this, "age", {
+        get() {
+          let todayYear = new Date().getFullYear();
+          return todayYear - this.birthday.getFullYear();
+        },
+        set(value) {
+              let now = new Date();
+          let yearOfBirth = now.getFullYear() - value;
+          this.birthday = new Date(yearOfBirth, now.getMonth(), now.getDate())
         }
-    })
-}
+      })
+    };
+    User.age = 35
+    
 let newUser = new User("Sanjay", new Date(1992, 6, 1));
 console.log(newUser.birthday);
 console.log(newUser.age)
